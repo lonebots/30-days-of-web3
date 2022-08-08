6c70b22177d94c0515ea964950a7b7d6cc369e45
lonebots <jishnusurajila@gmail.com>
2022-08-08 23:50:55 +0530

    check in attendees

--- web3rsvp/contracts/Web3RSVP.sol
+++ web3rsvp/contracts/Web3RSVP.sol
@@ -115,6 +115,45 @@ function confirmAttendee(bytes32 eventId, address attendee) public {
     require(sent, "Failed to send Ether");
 }
 
+function confirmAttendee(bytes32 eventId, address attendee) public {
+    // look up event from our struct using the eventId
+    CreateEvent storage myEvent = idToEvent[eventId];
 
+    // require that msg.sender is the owner of the event - only the host should be able to check people in
+    require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+    // require that attendee trying to check in actually RSVP'd
+    address rsvpConfirm;
+
+    for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+        if(myEvent.confirmedRSVPs[i] == attendee){
+            rsvpConfirm = myEvent.confirmedRSVPs[i];
+        }
+    }
+
+    require(rsvpConfirm == attendee, "NO RSVP TO CONFIRM");
+
+
+    // require that attendee is NOT already in the claimedRSVPs list AKA make sure they haven't already checked in
+    for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+        require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
+    }
+
+    // require that deposits are not already claimed by the event owner
+    require(myEvent.paidOut == false, "ALREADY PAID OUT");
+
+    // add the attendee to the claimedRSVPs list
+    myEvent.claimedRSVPs.push(attendee);
+
+    // sending eth back to the staker `https://solidity-by-example.org/sending-ether`
+    (bool sent,) = attendee.call{value: myEvent.deposit}("");
+
+    // if this fails, remove the user from the array of claimed RSVPs
+    if (!sent) {
+        myEvent.claimedRSVPs.pop();
+    }
+
+    require(sent, "Failed to send Ether");
+}
 
 }
